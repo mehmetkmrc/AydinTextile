76aae45e3e9f69b5de4eadf330d99cfd23561c3f
Mehmet <mehmetKmrc1109@gmail.com>
2023-08-25 11:36:49 +0300

    Add project files.

---
+++ AydinTextile/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AydinTextile.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Aydın Tekstil | Hakkımızda";
+            return View();
+        }
+    }
+}
---
+++ AydinTextile/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AydinTextile.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Aydın Kumaşçılık";
+            return View();
+        }
+        public ActionResult Slider()
+        {
+            return View();
+        }
+        public ActionResult Info()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult Counter()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+
+    }
+}
