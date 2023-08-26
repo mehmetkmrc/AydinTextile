d2a33e90a3c893e1beee1d8da5f65eddd9fca947
Mehmet <mehmetKmrc1109@gmail.com>
2023-08-26 03:46:21 +0300

    Project sayfası oluşturuldu.

---
+++ AydinTextile/Controllers/ProjectController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AydinTextile.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Aydın Tekstil | Çalışmalarımız";
+            return View();
+        }
+    }
+}
