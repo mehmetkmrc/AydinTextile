bbcc41565ba3c1cfdc6ea084e8b22c742598759a
Mehmet <mehmetKmrc1109@gmail.com>
2023-08-26 17:44:31 +0300

    AuthurAdmin kısmı eklendi

---
+++ AydinTextile/Controllers/AuthorAdminController.cs
@@ -0,0 +1,19 @@
+﻿using AydinTextile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AydinTextile.Controllers
+{
+    public class AuthorAdminController : Controller
+    {
+        // GET: AuthorAdmin
+        AydinTextileDBEntities db = new Models.AydinTextileDBEntities();
+        public ActionResult Index()
+        {
+            return View(db.Author.ToList());
+        }
+    }
+}
