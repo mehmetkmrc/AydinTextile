2600c8f9cd870e797a5970df85be12bf9836b841
Mehmet <mehmetKmrc1109@gmail.com>
2023-08-26 15:49:26 +0300

    Admin Panel Projeye Eklendi

---
+++ AydinTextile/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using AydinTextile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AydinTextile.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        AydinTextileDBEntities db = new Models.AydinTextileDBEntities();
+        public ActionResult Index()
+        {
+            var model = db.Author.ToList();
+            return View();
+        }
+    }
+}
